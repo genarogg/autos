76825e371179a0fdf69cd3f8606e2e7293cd930d
genarogg <genarrogg@gmail.com>
2024-02-22 20:55:05 -0400

    funciona agregar marca

---
+++ js/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDE_G6rXST9WRqHgkYeRGxCloxthsnK__Y",
+  authDomain: "autos-a2c3b.firebaseapp.com",
+  projectId: "autos-a2c3b",
+  storageBucket: "autos-a2c3b.appspot.com",
+  messagingSenderId: "723960148448",
+  appId: "1:723960148448:web:91183bd51325568622bb85",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
